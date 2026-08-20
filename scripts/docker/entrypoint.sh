9a7f144f5e29f712b32c50288615294fa9d96bac
isinterns <isinterns@seriindia.org>
2026-08-20 18:29:25 +0530

    fix(docker): migrate dockerfile and scripts for react-router spa build

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -x
 
-# Replacing placeholder urls to runtime variables, since we're using rewrites in nextjs, this is required.
+# Replacing placeholder urls to runtime variables, since we're using a static SPA
 # Everything else which doesn't compile URLs at build should already be able to use runtime variables.
 
-/app/scripts/replace-placeholder.sh "http://REPLACE-BACKEND-URL.com" "$NEXT_PUBLIC_BACKEND_URL"
-/app/scripts/replace-placeholder.sh "http://REPLACE-APP-URL.com" "$NEXT_PUBLIC_APP_URL"
+/app/scripts/replace-placeholder.sh "http://REPLACE-BACKEND-URL.com" "$VITE_PUBLIC_BACKEND_URL"
+/app/scripts/replace-placeholder.sh "http://REPLACE-APP-URL.com" "$VITE_PUBLIC_APP_URL"
 
-exec bun /app/apps/mail/server.js
+exec serve -s /app/apps/mail/build/client -l 3000
